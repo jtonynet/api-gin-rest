2e305c96f4204397bb2e0ab2d306a47a8392f886
jtonyNet <jtony.net@gmail.com>
2023-09-21 16:30:05 -0300

    feat: termino aula 4

--- main.go
+++ main.go
@@ -2,15 +2,10 @@ package main
 
 import (
 	"github.com/jtonynet/api-gin-rest/database"
-	"github.com/jtonynet/api-gin-rest/models"
 	"github.com/jtonynet/api-gin-rest/routes"
 )
 
 func main() {
 	database.ConectaComBancoDeDados()
-	models.Alunos = []models.Aluno{
-		{Nome: "Jonh Doe", CPF: "06335766889", RG: "47000000000"},
-		{Nome: "Marie Doe", CPF: "06335766880", RG: "48000000000"},
-	}
 	routes.HandleRequests()
 }

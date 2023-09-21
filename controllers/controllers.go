45e12837d66a8d385345b705e7d5855f30a97f56
jtonyNet <jtony.net@gmail.com>
2023-09-21 16:07:09 -0300

    feta: termino aula 03

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,7 +1,10 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/jtonynet/api-gin-rest/database"
 	"github.com/jtonynet/api-gin-rest/models"
 )
 
@@ -14,4 +17,15 @@ func Saudacao(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"API diz:": "E ai " + nome + ", tudo beleza?",
 	})
+}
+
+func CriaNovoAluno(c *gin.Context) {
+	var aluno models.Aluno
+	if err := c.ShouldBindJSON(&aluno); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+	database.DB.Create(&aluno)
+	c.JSON(http.StatusOK, aluno)
 }
